93bb58770d8b8bf818eab90b8d0412eeae50c8c0
Toydotgame <MatthewJPersonalAcct@outlook.com>
2021-01-05 18:25:38 +1000

    Doin' more things

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,19 @@ import java.io.IOException;
 public class Main {
 	static File saveFile = new File("save");
 	
-	static int x = 1;
-	static int y = 2;
-	static int z = 3;
+	
 
 	public static void main(String[] args) {
 		createFile();
+		writeToFile();
 	}
 	
-	public static void createFile() {
+	public static boolean createFile() {
 		boolean fileAlreadyExists = saveFile.exists();
 		
 		if(fileAlreadyExists == true) {
 			System.out.println("The file \"" + saveFile + "\" already exists!");
+			return true;
 		} else {
 			try { // I HATE try/catch loops!
 				if(saveFile.createNewFile()) {
@@ -27,15 +27,26 @@ public class Main {
 				System.out.println("Couldn't create new file!");
 				e.printStackTrace();
 			} // Burn. Slowly.
+			return false;
 		}
 	}
+	
+	// These should go before the `write();` block.
+	static int x = 1;
+	static int y = 2;
+	static int z = 3;
 	
 	public static void writeToFile() {
 		try { // try/catch can actually leave now, it's okay. The joke's over.
 			FileWriter writer = new FileWriter(saveFile);
-			writer.write("x: " + String.valueOf(x));
+			writer.write("x: " + String.valueOf(x) + "\n");
+			writer.write("y: " + String.valueOf(y) + "\n");
+			writer.write("z: " + String.valueOf(z) + "\n");
+			writer.close(); // Good practices, good sir.
+			System.out.println("Successfully wrote to file!");
 		} catch(IOException e) { // Leave.
-			
+			System.out.println("Couldn't write to file! (" + saveFile + ")");
+			e.printStackTrace();
 		}
 	}
 }
